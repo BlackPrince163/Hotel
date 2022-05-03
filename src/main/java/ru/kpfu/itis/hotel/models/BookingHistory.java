7a6ab029c48b7d7f40a8afe8b19bbf19a72927d1
BlackPrince163 <4523120100Ni>
2022-05-03 12:41:24 +0300

    + booking history model

---
+++ src/main/java/ru/kpfu/itis/hotel/models/BookingHistory.java
@@ -0,0 +1,39 @@
+package ru.kpfu.itis.hotel.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * 03.05.2022
+ * Hotel
+ *
+ * @author Niyaz Khadimullin @BlackPrince163
+ * 11-004
+ */
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "booking_hotel")
+public class BookingHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "room_id")
+    private Room room;
+    private Date dateFrom;
+    private Date dateTo;
+}
