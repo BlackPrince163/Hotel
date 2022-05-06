497d5e59c67f79c658819b30e9de65c9f85bcc6a
BlackPrince163 <4523120100Ni>
2022-05-06 17:31:37 +0300

    + user service & repositories

---
+++ src/main/java/ru/kpfu/itis/hotel/repositories/UsersRepository.java
@@ -0,0 +1,40 @@
+package ru.kpfu.itis.hotel.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+import ru.kpfu.itis.hotel.models.User;
+
+import java.util.Optional;
+
+/**
+ * 03.05.2022
+ * Hotel
+ *
+ * @author Niyaz Khadimullin @BlackPrince163
+ * 11-004
+ */
+
+public interface UsersRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    @Transactional
+    @Modifying
+    @Query(nativeQuery = true, value = "DELETE FROM user_hotel WHERE email = :email")
+    void deleteByEmail(String email);
+
+    @Transactional
+    @Modifying
+    @Query(nativeQuery = true, value = "UPDATE user_hotel " +
+            "SET first_name = :newFirstName, last_name = :newLastName WHERE email = :email")
+    void updateFirstNameAndLastNameByEmail(@Param("newFirstName") String newFirstName,
+                                           @Param("newLastName") String newLastName,
+                                           @Param("email") String email);
+
+    @Query(nativeQuery = true, value = "SELECT * FROM user_hotel WHERE email = :email")
+    Optional<User> findOneByEmail(@Param("email") String email);
+
+}
